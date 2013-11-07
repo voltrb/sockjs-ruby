4cbb23460ed509e9503692a9bba5e0b8c2b44ad4
Kacper Kawecki <kacper@geniebelt.com>
2013-11-07 13:34:40 +0100

    added more debug info on session close error

--- lib/sockjs/transports/websocket.rb
+++ lib/sockjs/transports/websocket.rb
@@ -107,7 +107,7 @@ module SockJS
           begin
             session.close
           rescue Object => ex
-            SockJS::debug "Error closing websocket (#{event.inspect[0..40]}): #{ex.inspect}"
+            SockJS::debug "Error closing websocket (#{event.inspect[0..40]}): #{ex.inspect} \n#{ex.message} \n#{ex.backtrace.inspect}"
           end
         end
       end
