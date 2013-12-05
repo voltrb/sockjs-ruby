65d538f7aa7bbaf2bf284ea4f8da22a639579889
Kacper Kawecki <kacper@geniebelt.com>
2013-12-06 00:25:52 +0100

    Bumped version

--- lib/sockjs/faye.rb
+++ lib/sockjs/faye.rb
@@ -15,11 +15,4 @@ end
 class Thin::Request
   WEBSOCKET_RECEIVE_CALLBACK = 'websocket.receive_callback'.freeze
   GET = 'GET'.freeze
-
-  #def websocket?
-  #  @env['REQUEST_METHOD'] == GET and
-  #  @env['HTTP_CONNECTION'] and
-  #  @env['HTTP_CONNECTION'].split(/\s*,\s*/).include?('Upgrade') and
-  #  ['WebSocket', 'websocket'].include?(@env['HTTP_UPGRADE'])
-  #end
 end
